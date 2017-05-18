b963d716cce24644ca99fa650282aa1204c7aaee
Artem Kaloev <kaloev.eltech@gmail.com>
2017-05-18 19:37:45 +0300

    Enterpoint

---
+++ cmake-build-debug/CMakeFiles/CoursePublic.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CoursePublic.dir/main.c.o"
+  "CoursePublic.pdb"
+  "CoursePublic"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CoursePublic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
